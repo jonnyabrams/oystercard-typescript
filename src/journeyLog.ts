7815cfc015f5561169bd9ca7cc3b60bf5d947c6a
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-04 14:09:29 +0100

    Create JourneyLog class with tests

---
+++ src/journeyLog.ts
@@ -0,0 +1,22 @@
+import Journey from './journey'
+
+export default class JourneyLog {
+  currentJourney: Journey
+  journeys: { entryStation: string, exitStation: string, fare: number }[]
+  
+  constructor(journeyClass: Journey) {
+    this.currentJourney = journeyClass
+    this.journeys = []
+  }
+
+  start(entryStation: string) {
+    this.currentJourney.startJourney(entryStation)
+  }
+
+  finish(exitStation: string) {
+    this.currentJourney.endJourney(exitStation)
+    this.journeys.push({ entryStation: this.currentJourney.entryStation, exitStation: this.currentJourney.exitStation, fare: this.currentJourney.fareCharged() })
+  }
+
+  
+}
---
+++ test/journeyLog.test.ts
@@ -0,0 +1,36 @@
+import JourneyLog from '../src/journeyLog'
+import Journey from '../src/journey'
+
+describe('JourneyLog', () => {
+  let journeyLog: JourneyLog
+
+  beforeEach(() => {
+    journeyLog = new JourneyLog(new Journey)
+  })
+
+  it('initializes with an empty array for storing journeys', () => {
+    expect(journeyLog.journeys).toEqual([])
+  })
+
+  describe('start', () => {
+    it('starts a new journey and records the entry station', () => {
+      journeyLog.start('Camden')
+      expect(journeyLog.currentJourney.entryStation).toEqual('Camden')
+    })
+  })
+
+  describe('finish', () => {
+    it('finishes a journey and records the exit station', () => {
+      journeyLog.start('Camden')
+      journeyLog.finish('Euston')
+      expect(journeyLog.currentJourney.exitStation).toEqual('Euston')
+    })
+
+    it('stores the completed journey in the journeys array', () => {
+      journeyLog.start('Camden')
+      journeyLog.finish('Euston')
+      expect(journeyLog.journeys).toEqual([{ entryStation: 'Camden', exitStation: 'Euston', fare: 1 }])
+    })
+  })
+
+})
